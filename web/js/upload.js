dd3b7d101bebfac6837a5c8f0af4543d4add56bb
Tobias Hahn <hahn.tobias66@gmail.com>
2016-06-23 22:26:03 +0200

    adde dynamic setting of upload-url

--- web/js/upload.js
+++ web/js/upload.js
@@ -1,31 +1,3 @@
-var FormEvaluator = (function () {
-    function FormEvaluator() {
-        var _this = this;
-        this.jquery = jQuery;
-        this.checkFormData = function () {
-            var formContents = _this.jquery('#fileToLoad');
-            console.log(formContents);
-            _this.jquery.post({
-                url: 'http://localhost:8667/upload',
-                data: formContents,
-                error: function (data, text) {
-                    console.log(data);
-                    console.log(text);
-                }
-            });
-        };
-    }
-    FormEvaluator.prototype.init = function () {
-        var _this = this;
-        jQuery('document').ready(function () {
-            var button = _this.jquery('#subm');
-            button.click(function (clickEvent) {
-                clickEvent.preventDefault();
-            });
-            button.click(_this.checkFormData);
-        });
-    };
-    return FormEvaluator;
-}());
-var formEv = new FormEvaluator();
-//# sourceMappingURL=upload.js.map
+$(document).ready(function{
+	$('#uploadForm').attr('action', UPLOAD_CONFIG.HELOISE_PARISIENS_UPLOAD_URL);
+});
